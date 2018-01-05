141de6c92f8fa7eb8fcb98ad78e68e7f530c8dca
nissim ohayon <nissimmoha@gmail.com>
2018-01-05 10:31:38 +0200

    SingletonPattern
    By C# exemple

---
+++ Singleton/Nissim.On.Singleton/Nissim.On.Singleton/GodSingleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Nissim.On.Singleton
+{
+    public class GodSingleton
+    {
+        private GodSingleton(string p_name, bool p_isGood)
+        {
+            Name = p_name;
+            IsGood = p_isGood;
+        }
+
+        public static GodSingleton GetGodInstance()
+        {
+            if(GodInstance == null)
+            {
+                GodInstance = new GodSingleton("God", true);
+            }
+            return GodInstance;
+        }
+
+        private static GodSingleton GodInstance;
+        public string Name { get; private set; }
+        public bool IsGood { get; private set; }
+    }
+}
---
+++ Singleton/Nissim.On.Singleton/Nissim.On.Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nissim.On.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var god = GodSingleton.GetGodInstance();
+
+            Console.WriteLine("Name: {0}\nIsGood: {1}", god.Name, god.IsGood);
+        }
+    }
+}
